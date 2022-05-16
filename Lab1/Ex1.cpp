74bd6471fcbd8e2eacea7d9156c68e5bd690c4e7
kcuriel-nts <kcuriel@neural.com.do>
2022-05-16 00:03:37 -0400

    Add excercise 6

--- Lab1/Ex1.cpp
+++ Lab1/Ex1.cpp
@@ -6,10 +6,31 @@ using namespace std;
 
 int main() {
 
-	Account account1{ "Jane Green" };
-	Account account2{ "Kevin Curiel" };
+	Account account1{ "Jane Green" , 50};
+	Account account2{ "Kevin Curiel" , -7};
 
-	cout << "Account 1 name is: " << account1.getName() << endl;
-	cout << "Account 2 name is: " << account2.getName() << endl;
+	cout << "Account 1 name is: " << account1.getName() << " balance is $" << account1.getBalance();
+	cout << "Account 2 name is: " << account2.getName() << " balance is $" << account2.getBalance();
 
+	cout << "\n\nEnter deposit amount for accoutn1: ";
+	int depositAmount;
+
+	cin >> depositAmount;
+
+	cout << "Adding " << depositAmount << " to account1 balance";
+	account1.deposit(depositAmount);
+
+	cout << "Account 1 name is: " << account1.getName() << " balance is $" << account1.getBalance();
+	cout << "Account 2 name is: " << account2.getName() << " balance is $" << account2.getBalance();
+
+	cout << "\n\nEnter deposit amount for accoutn1: ";
+	int depositAmount;
+
+	cin >> depositAmount;
+
+	cout << "Adding " << depositAmount << " to account1 balance";
+	account1.deposit(depositAmount);
+
+	cout << "Account 1 name is: " << account1.getName() << " balance is $" << account1.getBalance();
+	cout << "Account 2 name is: " << account2.getName() << " balance is $" << account2.getBalance();
 }
