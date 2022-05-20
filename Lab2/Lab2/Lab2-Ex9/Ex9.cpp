2b56419242fb7a4820150529f51999daae1e6966
kcurielj <kcurielj@my.centennialcollege.ca>
2022-05-19 23:50:23 -0400

    complete the excercises

---
+++ Lab2/Lab2/Lab2-Ex9/Ex9.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+inline double cube(const double side) {
+	return side * side * side;
+}
+
+int main() {
+	double sideValue;
+	cout << "Enter the side length of your cube";
+	cin >> sideValue;
+
+	cout << "Valume of cube with side "
+		<< sideValue << " is " << cube(sideValue) << endl;
+}
